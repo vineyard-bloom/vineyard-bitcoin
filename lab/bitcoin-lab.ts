83661224eae32e23f54ae540137d4193fe875f25
cj <christopherwjohnson@gmail.com>
2017-09-20 14:39:07 -0600

    Move bitcoin-lab from its own repo and into this repo.

---
+++ lab/bitcoin-lab.ts
@@ -0,0 +1,98 @@
+import {BitcoinServer} from "./bitcoin-server"
+import {BitcoinClient, satoshisToBitcoin} from "../src"
+const child_process = require('child_process')
+const fs = require('fs')
+const rimraf = require('rimraf')
+
+export interface BitcoinLabConfig {
+  walletPath: string
+}
+
+export class BitcoinLab {
+  server: BitcoinServer
+  client: BitcoinClient
+  config: BitcoinLabConfig
+
+  constructor(config: BitcoinLabConfig, client: BitcoinClient, server: BitcoinServer = new BitcoinServer()) {
+    this.config = config
+    this.client = client
+    this.server = server
+  }
+
+  private deleteLock(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      fs.unlink(this.config.walletPath + '/' + '.lock', function (result, error, stdout, stderr) {
+        if (error)
+          reject(error)
+        else
+          resolve(result)
+      })
+    })
+  }
+
+  deleteWallet(): Promise<any> {
+    console.log('Deleting regtest bitcoin wallet')
+    return this.deleteLock()
+      .then(() => new Promise((resolve, reject) => {
+          rimraf(this.config.walletPath, function (error, stdout, stderr) {
+            if (error)
+              reject(error)
+
+            else
+              resolve(stdout)
+          })
+        })
+      )
+  }
+
+  start(): Promise<any> {
+    return this.server.start()
+  }
+
+  stop(): Promise<any> {
+    return this.server.stop()
+  }
+
+  reset(): Promise<any> {
+    return this.deleteWallet()
+    // return this.stop()
+      // .then(() => this.deleteWallet())
+      // .then(() => this.start())
+  }
+
+  generate(blockCount: number): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.client.getClient().generate(blockCount, (error) => {
+        if (error)
+          reject(new Error(error));
+        else
+          resolve()
+      })
+    })
+  }
+
+  send(address: string, amount: number){
+    return new Promise<void>((resolve, reject) => {
+      this.client.getClient().sendToAddress(address, satoshisToBitcoin(amount), (error) => {
+        if (error)
+          reject(new Error(error));
+        else
+          resolve()
+      })
+    })
+  }
+
+  sendMany(addressAmounts) {
+    return new Promise<void>((resolve, reject) => {
+      for (var i in addressAmounts) {
+        addressAmounts[i] = satoshisToBitcoin(addressAmounts[i])
+      }
+      this.client.getClient().sendMany('', addressAmounts, (error) => {
+        if (error)
+          reject(new Error(error));
+        else
+          resolve()
+      })
+    })
+  }
+}
---
+++ lab/bitcoin-server.ts
@@ -0,0 +1,64 @@
+const child_process = require('child_process')
+
+enum Status {
+  inactive,
+  active
+}
+
+function waitUntilRunning() {
+  return new Promise<void>((resolve, reject) => {
+    const poll = () => {
+      child_process.exec('bitcoin-cli getinfo', function (error, stdout, stderr) {
+        if (error) {
+          // console.log('not yet', stderr)
+          setTimeout(poll, 100)
+        }
+        else {
+          console.log('Bitcoind is now running')
+          resolve()
+        }
+      })
+    }
+
+    setTimeout(poll, 3000)
+  })
+}
+
+export class BitcoinServer {
+  private status: Status = Status.inactive
+  private stdout
+  private stderr
+  private childProcess
+
+  start() {
+    console.log('Starting bitcoind')
+    const childProcess = this.childProcess = child_process.spawn('bitcoind')
+
+    childProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    childProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    childProcess.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    })
+
+    return waitUntilRunning()
+  }
+
+  stop() {
+    if (!this.childProcess)
+      return Promise.resolve()
+
+    return new Promise((resolve, reject) => {
+      this.childProcess.kill()
+      this.childProcess.on('close', (code) => {
+        resolve()
+      })
+    })
+
+  }
+}
